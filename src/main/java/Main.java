2d6b16c94071d4282e4ca0bdc96308d492acdadb
Pinky Raj <95484162+Pinkyraj9977@users.noreply.github.com>
2024-04-14 00:24:06 +0530

    Add files via upload

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args){
+        SUV fortuner = new SUV("fortuner", false);
+        fortuner.move(40, 0);
+        fortuner.accelerate(20);
+        //fortuner.accelerate(-60);
+        System.out.println("Current gear: "+ fortuner.getCurrentGear());
+        System.out.println("Current gear: "+ fortuner.getCurrentSpeed());
+
+    }
+}
